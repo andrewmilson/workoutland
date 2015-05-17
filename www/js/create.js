c85c9216559f5220b4a05e60d954a2e12c1f5694
Andrew Milson <andrew.j.milson@gmail.com>
2015-05-17 18:10:26 +1200

    Base of app finished. Added TTS.

--- www/js/create.js
+++ www/js/create.js
@@ -1,15 +1,34 @@
 angular.module('createController', [])
 
 .controller('createController',
-['$scope',
-function($scope) {
-  $scope.steps = [{}];
-
-  $scope.addExercise = function() {
-    $scope.steps.push({});
+['$scope', '$http',
+function($scope, $http) {
+  $scope.workout = {
+    name: '',
+    steps: [{}]
   };
 
-  $scope.remove = function($index) {
-    $scope.steps.splice($index, 1);
+  $scope.addStep = function() {
+    $scope.workout.steps.push({});
   };
+
+  $scope.removeStep = function($index) {
+    $scope.workout.steps.splice($index, 1);
+  };
+
+  $scope.createWorkout = function() {
+    $scope.createDisabled = true;
+    var workout = angular.copy($scope.workout);
+
+    workout.steps.map(function(step) {
+      step.time = (step.mins || 0) * 60 + (step.seconds || 0);
+      delete step.mins;
+      delete step.seconds;
+    });
+
+    $http.post('http://calisthenics-timer.appspot.com/', workout)
+    .success(function(data) {
+      location.replace('#/' + data);
+    });
+  }
 }]);
---
+++ www/js/workouts.js
@@ -0,0 +1,20 @@
+angular.module('workoutsController', [])
+
+.controller('workoutsController',
+['$scope', '$timeout', '$state', '$http',
+function($scope, $timeout, $state, $http) {
+  $scope.workouts = [];
+
+  for (var key in localStorage) {
+    if (!~localStorage[key].indexOf('workout-')) {
+      $scope.workouts.push(JSON.parse(localStorage[key]))
+      var workout = $scope.workouts.slice(-1)[0];
+      workout.time = 0;
+      workout.steps.map(function(a) {
+        workout.time += a.time;
+      });
+    }
+  }
+
+  console.log($scope.workouts);
+}]);
