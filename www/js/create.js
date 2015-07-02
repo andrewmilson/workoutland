5fcb151baa8f0b96ae39c86cdd2b0136780ad403
Andrew Milson <andrew.j.milson@gmail.com>
2015-07-02 22:33:35 +1200

    MAJOR UPDATE. Changed style.

--- www/js/create.js
+++ www/js/create.js
@@ -3,14 +3,15 @@ angular.module('createController', [])
 .controller('createController',
 ['$scope', '$http',
 function($scope, $http) {
-  $scope.workout = {
+  var workout = $scope.workout = {
     name: '',
     notes: '',
-    steps: [{}]
+    steps: [{}],
+    temp: true
   };
 
   $scope.addStep = function() {
-    $scope.workout.steps.push({});
+    workout.steps.push({});
     setTimeout(function() {
       $('.step-name').focus();
       window.scrollTo(0, document.body.scrollHeight);
@@ -18,15 +19,26 @@ function($scope, $http) {
   };
 
   $scope.removeStep = function($index) {
-    $scope.workout.steps.splice($index, 1);
+    workout.steps.splice($index, 1);
   };
 
   $scope.createWorkout = function() {
     $scope.createDisabled = true;
+    var tempId = Math.floor(Math.random() * 1000000000).toString(36);
+    workout.id = tempId;
+    localStorage['workout-' + tempId] = JSON.stringify(workout);
 
-    $http.post('http://workout-land.appspot.com/', $scope.workout)
+    $http.post('http://workout-land.appspot.com/', workout)
     .success(function(data) {
+      var tempWorkout = JSON.parse(localStorage['workout-' + tempId]);
+      tempWorkout.id = data;
+      delete tempWorkout.temp;
+      localStorage.remove('workout-' + tempId)
+      localStorage['workout-' + data] = JSON.stringify(tempWorkout);
       location.replace('#/' + data);
+    })
+    .error(function() {
+      location.replace('#/' + tempId);
     });
   }
 }]);
